ce859d4e6fea45ed74b6176613d0e7fd0f087a8f
Chris <chris79gp@outlook.com>
2025-07-16 15:36:49 +0100

    Feat: add styling for desktop navigation

--- src/components/Navigation/NavItem/NavItem.tsx
+++ src/components/Navigation/NavItem/NavItem.tsx
@@ -5,31 +5,40 @@ import { NavLink, type NavLinkProps } from "react-router-dom";
 interface NavItemProps {
 	to: string;
 	name: string;
+	isCta?: boolean;
 }
 
 type RouterNavLinkProps = NavLinkProps & {
 	ref?: React.Ref<HTMLAnchorElement>;
+	isCta: boolean;
 };
 
 const RouterNavLink = (props: RouterNavLinkProps) => {
-	const { ref, ...rest } = props;
+	const { ref, isCta, ...rest } = props;
 
 	return (
 		<NavLink
 			ref={ref}
 			{...rest}
 			className={({ isActive, isPending }) =>
-				clsx("nav__link", { "nav__link--active": isActive, "nav__link--pending": isPending })
+				clsx("nav__link", {
+					"nav__link--active": isActive,
+					"nav__link--pending": isPending,
+					"nav__link--cta": isCta,
+				})
 			}
 		/>
 	);
 };
 
-const NavItem = function ({ to, name }: NavItemProps) {
+const NavItem = function ({ to, name, isCta = false }: NavItemProps) {
 	return (
+		// <NavigationMenu.Item className="nav__list-item">
 		<NavigationMenu.Item className="nav__list-item">
 			<NavigationMenu.Link asChild>
-				<RouterNavLink to={to}>{name}</RouterNavLink>
+				<RouterNavLink to={to} isCta={isCta}>
+					{name}
+				</RouterNavLink>
 			</NavigationMenu.Link>
 		</NavigationMenu.Item>
 	);
