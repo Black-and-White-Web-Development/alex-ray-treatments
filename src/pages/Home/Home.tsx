ec9eb19305aa72e2b2ebeaa0997ca722b95b334b
Chris <chris79gp@outlook.com>
2025-08-23 19:16:49 +0100

    Feat: add summary of treatments to homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,14 @@
 import TextWithImage from "@/components/TextWithImage";
 
+import type { Service as ServiceType } from "@/types/services.types";
+
 import portrait from "@/assets/alex-ray-working.webp";
+import servicesData from "@/data/treatments.json" assert { type: "json" };
+import { strToKebabCase } from "@/util/strToKebabCase";
+
+import "./Home.scss";
+
+const services = servicesData as ServiceType[];
 
 const Home = function () {
 	return (
@@ -17,6 +25,31 @@ const Home = function () {
 					"Porro dolorum reiciendis fugit! Deserunt corporis ipsa doloremque accusamus optio, neque aperiam similique nostrum error.",
 				]}
 			/>
+			<section className="treatments-summary fb-col-wrapper">
+				<h2 className="treatments-summary__heading">Alex's wellbeing treatments</h2>
+				<ul className="treatments-summary__list">
+					{services.map(service => (
+						<li key={service.id} className="treatments-summary__list-item">
+							<a className="summary__link" href={`/treatments#${strToKebabCase(service.heading)}`}>
+								<article className="summary">
+									<header className="summary__header">
+										<div className="summary__heading-container">
+											<h3 className="summary__heading">{service.heading}</h3>
+											<p className="summary__subheading">View treatments ⭢</p>
+										</div>
+										<img
+											className="summary__image"
+											src={service.image.url}
+											alt={service.image.alt}
+										/>
+									</header>
+									<p className="summary__body">{service.body[1]}</p>
+								</article>
+							</a>
+						</li>
+					))}
+				</ul>
+			</section>
 		</>
 	);
 };
---
+++ src/util/strToKebabCase.ts
@@ -0,0 +1,1 @@
+export const strToKebabCase = (str: string) => str.trim().toLowerCase().replace(/\s+/g, "-");
