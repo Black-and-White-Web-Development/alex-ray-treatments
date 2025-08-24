aa267e289c3f1d1b97996dd8f76de64536dc7681
Chris <chris79gp@outlook.com>
2025-08-24 14:31:07 +0100

    Feat: update homepage welcome and make accessibility improvements around the homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import TextWithImage from "@/components/TextWithImage";
 
 import type { Service as ServiceType } from "@/types/services.types";
 
-import portrait from "@/assets/alex-ray-working.webp";
+import sign from "@/assets/alex-ray-wellbeing-sign.webp";
 import servicesData from "@/data/treatments.json" assert { type: "json" };
 import { strToKebabCase } from "@/util/strToKebabCase";
 
@@ -15,15 +15,15 @@ const Home = function () {
 		<>
 			<TextWithImage
 				position="right"
-				image={{ url: portrait, alt: "Alex Ray" }}
+				image={{ url: sign, alt: "Alex Ray" }}
 				heading="Welcome to Alex Ray Treatments"
 				text={[
 					"Tucked away in a quiet corner of West Sussex, Alex Ray Treatments offers a haven of calm, comfort, and expert care.",
 					"Alex has an attentive, personal approach and loves nothing better than watching her clients switch off from the world and truly relax during their appointments.",
 					"Alex is passionate about championing natural, cruelty-free products. She holds a Level 4 diploma in Beauty Therapy and Salon Management.",
-					"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga molestiae recusandae nam laudantium pariatur fugiat repudiandae reprehenderit! Officia, ut.",
-					"Porro dolorum reiciendis fugit! Deserunt corporis ipsa doloremque accusamus optio, neque aperiam similique nostrum error.",
+					"The salon is open six days a week, with flexible opening hours including some evenings. Alex also provides her specialist massage treatments to guests at The Pig Hotel near Arundel.",
 				]}
+				cta={{ href: "about", label: "Find out more about Alex" }}
 			/>
 			<section className="treatments-summary fb-col-wrapper">
 				<h2 className="treatments-summary__heading">Alex's wellbeing treatments</h2>
@@ -43,7 +43,7 @@ const Home = function () {
 											alt={service.image.alt}
 										/>
 									</header>
-									<p className="summary__body">{service.body[1]}</p>
+									<p className="summary__body">{service.body[0]}</p>
 								</article>
 							</a>
 						</li>
